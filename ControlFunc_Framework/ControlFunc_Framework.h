d9df0f47b076542761b71b74ae69f97ebfcc54ad
Eito Moriuchi <emoriuchi08@gmail.com>
2020-04-25 14:25:33 +0900

    Initial Commit

---
+++ ControlFunc_Framework/ControlFunc_Framework.h
@@ -0,0 +1,19 @@
+//
+//  ControlFunc_Framework.h
+//  ControlFunc_Framework
+//
+//  Created by 森内　映人 on 2020/04/25.
+//  Copyright © 2020 森内　映人. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ControlFunc_Framework.
+FOUNDATION_EXPORT double ControlFunc_FrameworkVersionNumber;
+
+//! Project version string for ControlFunc_Framework.
+FOUNDATION_EXPORT const unsigned char ControlFunc_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ControlFunc_Framework/PublicHeader.h>
+
+
